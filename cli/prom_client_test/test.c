5c4137341524e89acbd7ff7b0d0c0c4cf6b107bf
calmwu <wubo0067@hotmail.com>
2022-05-08 00:01:41 +0800

    fix: porm keys remove

--- cli/prom_client_test/test.c
+++ cli/prom_client_test/test.c
@@ -14,14 +14,39 @@
 #include "prometheus-client-c/prom_map_i.h"
 #include "prometheus-client-c/prom_metric_t.h"
 #include "prometheus-client-c/prom_metric_i.h"
+#include "prometheus-client-c/prom_linked_list_i.h"
 
 static prom_gauge_t   *bar_gauge;
 static prom_counter_t *bar_counter;
 
 static const char *__key = "UdpLite";
 
+static void __show_collectors(prom_map_t *collectors) {
+    int32_t index = 0;
+    for (prom_linked_list_node_t *current_node = collectors->keys->head; current_node != NULL;
+         current_node = current_node->next, index++) {
+        const char *collector_name = (const char *)current_node->item;
+        debug("PROM_COLLECTOR_REGISTRY_DEFAULT->collectors[%d] name: '%s'", index, collector_name);
+    }
+}
+
+static void __show_metrics(prom_map_t *metrics) {
+    int32_t index = 0;
+    for (prom_linked_list_node_t *current_node = metrics->keys->head; current_node != NULL;
+         current_node = current_node->next, index++) {
+        const char *metric_name = (const char *)current_node->item;
+        debug("metrics[%d] name: '%s'", index, metric_name);
+    }
+}
+
+static prom_linked_list_compare_t __compare_key(void *item_a, void *item_b) {
+    const char *str_a = (const char *)item_a;
+    const char *str_b = (const char *)item_b;
+    return strcmp(str_a, str_b);
+}
+
 int32_t main(int32_t argc, char **argv) {
-    if (log_init("../cli/prom_client_test/log.cfg", "prom_client_test") != 0) {
+    if (log_init("../cli/log.cfg", "prom_client_test") != 0) {
         fprintf(stderr, "log init failed\n");
         return -1;
     }
@@ -52,6 +77,11 @@ int32_t main(int32_t argc, char **argv) {
         error("prom_collector_registry_default_init failed, ret: %d", ret);
         return -1;
     }
+    // ** !!!!!!!!!!!!!!!!
+    // prom_linked_list_set_compare_fn(PROM_COLLECTOR_REGISTRY_DEFAULT->collectors->keys,
+    //                                 __compare_key);
+
+    __show_collectors(PROM_COLLECTOR_REGISTRY_DEFAULT->collectors);
 
     bar_gauge = prom_collector_registry_must_register_metric(
         prom_gauge_new("bar_gauge", "gauge for bar", 1, (const char *[]){ "label" }));
@@ -59,7 +89,6 @@ int32_t main(int32_t argc, char **argv) {
     bar_counter = prom_collector_registry_must_register_metric(
         prom_counter_new("bar_counter", "counter for bar", 0, NULL));
 
-    // prom_collector_t *test_collector = prom_collector_new("test");
     prom_collector_t *default_collector =
         (prom_collector_t *)prom_map_get(PROM_COLLECTOR_REGISTRY_DEFAULT->collectors, "default");
     if (unlikely(NULL == default_collector)) {
@@ -69,26 +98,47 @@ int32_t main(int32_t argc, char **argv) {
         debug("get PROM_COLLECTOR_REGISTRY_DEFAULT.collectors successed");
     }
 
-    debug("PROM_COLLECTOR_REGISTRY_DEFAULT.default collector have %ld metrics",
+    __show_metrics(default_collector->metrics);
+    debug("+++PROM_COLLECTOR_REGISTRY_DEFAULT.default collector have %ld metrics",
           prom_map_size(default_collector->metrics));
-
-    // 将这个collector注册到默认的collector registry
-    // prom_collector_registry_register_collector(PROM_COLLECTOR_REGISTRY_DEFAULT, test_collector);
-
-    //
-    // debug("after register test collector, current collector count:%ld",
-    //       prom_map_size(PROM_COLLECTOR_REGISTRY_DEFAULT->collectors));
 
     // 从map中释放这个指标
     prom_map_delete(default_collector->metrics, ((prom_metric_t *)bar_gauge)->name);
+    __show_metrics(default_collector->metrics);
+    debug("---PROM_COLLECTOR_REGISTRY_DEFAULT.default collector have %ld metrics",
+          prom_map_size(default_collector->metrics));
+
+    debug("-------------------------------------");
+
+    prom_collector_t *test_collector = prom_collector_new("test");
+
+    // 将这个collector注册到PROM_COLLECTOR_REGISTRY_DEFAULT.collectors中
+    prom_collector_registry_register_collector(PROM_COLLECTOR_REGISTRY_DEFAULT, test_collector);
+    __show_collectors(PROM_COLLECTOR_REGISTRY_DEFAULT->collectors);
+    debug("+++add test collector, %ld", prom_map_size(PROM_COLLECTOR_REGISTRY_DEFAULT->collectors));
+
+    // 将collector从PROM_COLLECTOR_REGISTRY_DEFAULT.collectors注销
+    prom_map_delete(PROM_COLLECTOR_REGISTRY_DEFAULT->collectors, test_collector->name);
+    __show_collectors(PROM_COLLECTOR_REGISTRY_DEFAULT->collectors);
+    debug("---delete test collector, %ld",
+          prom_map_size(PROM_COLLECTOR_REGISTRY_DEFAULT->collectors));
+
+    // -------------再次注册一个collector---------------------
+    test_collector = prom_collector_new("test");
+
+    prom_collector_registry_register_collector(PROM_COLLECTOR_REGISTRY_DEFAULT, test_collector);
+    __show_collectors(PROM_COLLECTOR_REGISTRY_DEFAULT->collectors);
+    debug("+++add test collector, %ld", prom_map_size(PROM_COLLECTOR_REGISTRY_DEFAULT->collectors));
+
+    // 将collector从PROM_COLLECTOR_REGISTRY_DEFAULT.collectors注销
+    prom_map_delete(PROM_COLLECTOR_REGISTRY_DEFAULT->collectors, test_collector->name);
+    __show_collectors(PROM_COLLECTOR_REGISTRY_DEFAULT->collectors);
+    debug("---delete test collector, %ld",
+          prom_map_size(PROM_COLLECTOR_REGISTRY_DEFAULT->collectors));
 
     // 销毁这个指标，这里不需要
     // prom_gauge_destroy(bar_gauge);
     // bar_gauge = NULL;
-
-    debug(
-        "after prom_map_delete PROM_COLLECTOR_REGISTRY_DEFAULT.default collector have %ld metrics",
-        prom_map_size(default_collector->metrics));
 
     // prom_collector_destroy(test_collector);
 
