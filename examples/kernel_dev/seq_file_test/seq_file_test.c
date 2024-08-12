fe4abb561aa280effe497b68eb09beb8cb302740
calm.wu <wubo0067@hotmail.com>
2024-08-12 17:46:49 +0800

    Implement displaying all running threads using seq_file.

--- examples/kernel_dev/seq_file_test/seq_file_test.c
+++ examples/kernel_dev/seq_file_test/seq_file_test.c
@@ -81,8 +81,8 @@ static void num_seq_stop(struct seq_file *s, void *v)
     if (v) {
         kfree(v);
         seq_printf(s, "End\n");
-        pr_info(MODULE_TAG " seq iteration stop<---\n");
     }
+    pr_info(MODULE_TAG " seq iteration stop<---\n");
 }
 
 static struct seq_operations num_seq_ops = {
@@ -112,55 +112,197 @@ static struct file_operations num_file_ops = {
     .release = seq_release,
 };
 
-static void *tasks_seq_start(struct seq_file *s, loff_t *pos)
+static const char *thrdlist_hdr =
+        "------------------------------------------------------------------------------------------\n"
+        "    TGID     PID         current           stack-start         Thread Name     MT? # thrds\n"
+        "------------------------------------------------------------------------------------------\n";
+
+struct thrds_seq_data {
+    struct task_struct *p;
+    struct task_struct *t;
+};
+
+static int32_t reach_end = 0;
+
+static void *thrds_seq_start(struct seq_file *s, loff_t *pos)
 {
-    struct task_struct *p = NULL;
+    loff_t index = *pos;
+    struct task_struct *p = NULL, *t = NULL;
+    struct thrds_seq_data *data = NULL;
+
+    if (*pos == 0) {
+        // print header
+        seq_printf(s, "%s", thrdlist_hdr);
+        data = kmalloc(sizeof(struct thrds_seq_data), GFP_KERNEL);
+        if (!data) {
+            return NULL;
+        }
+        data->p = &init_task;
+        data->t = &init_task;
+        pr_info(MODULE_TAG
+                " thrds seq iteration start pos:(%lld), task:%d--->\n",
+                *pos, data->p->pid);
+        return data;
+    } else {
+        if (*((int32_t *)(s->private)) == 1) {
+            pr_info(MODULE_TAG
+                    " thrds seq iteration next pos:(%lld) reach the task end\n",
+                    *pos);
+            return NULL;
+        }
+        // 从头跳过 index 个线程
+        do_each_thread(p, t)
+        {
+            index--;
+            if (index == 0) {
+                goto OUT;
+            }
+        }
+        while_each_thread(p, t);
+
+    OUT:
+        if (p != &init_task) {
+            data = kmalloc(sizeof(struct thrds_seq_data), GFP_KERNEL);
+            if (!data) {
+                return NULL;
+            }
+            data->p = p;
+            data->t = t;
+            pr_info(MODULE_TAG
+                    " thrds seq iteration next pos:(%lld), task:%d--->\n",
+                    *pos, data->p->pid);
+            return data;
+        }
+    }
 
     return NULL;
 }
 
-static void *tasks_seq_next(struct seq_file *s, void *v, loff_t *pos)
+static void *thrds_seq_next(struct seq_file *s, void *v, loff_t *pos)
 {
-    return NULL;
+    struct thrds_seq_data *data = (struct thrds_seq_data *)v;
+    struct task_struct *p = data->p;
+    struct task_struct *t = data->t;
+
+    data->t = next_thread(t);
+    if (data->t == p) {
+        data->p = next_task(p);
+        data->t = data->p;
+        pr_info(MODULE_TAG " thrds seq iteration, change to next task:(%d)\n",
+                data->p->pid);
+    } else {
+        pr_info(MODULE_TAG
+                " thrds seq iteration, the next thrd:(tgid:%d,pid:%d,thrid:%d)\n",
+                data->p->tgid, data->p->pid, data->t->pid);
+    }
+
+    if (*pos != 0 && data->p == &init_task) {
+        // 如果已经遍历完所有线程，则返回 NULL
+        kfree(data);
+        *((int32_t *)(s->private)) = 1;
+        pr_info(MODULE_TAG " thrds seq iteration reach the end\n");
+        return NULL;
+    }
+    // } else {
+    //     pr_info(MODULE_TAG " thrds seq iteration next pos:(%lld)\n", *pos);
+    // }
+
+    ++(*pos);
+    return data;
 }
 
-static void tasks_seq_stop(struct seq_file *s, void *v)
+static void thrds_seq_stop(struct seq_file *s, void *v)
 {
+    if (v) {
+        kfree(v);
+        pr_info(MODULE_TAG " thrds seq iteration free data\n");
+    }
+    pr_info(MODULE_TAG " thrds seq iteration stop<---\n");
     return;
 }
 
-static int32_t tasks_seq_show(struct seq_file *s, void *v)
+static int32_t thrds_seq_show(struct seq_file *s, void *v)
 {
+    int32_t nr_thrds = 0;
+
+    struct thrds_seq_data *data = (struct thrds_seq_data *)v;
+    struct task_struct *p = data->p;
+    struct task_struct *t = data->t;
+
+    get_task_struct(data->t);
+    task_lock(t);
+
+    pr_info(MODULE_TAG " thrds seq iteration show pid:(%d)\n", t->pid);
+    // 获取当前进程的线程数
+    nr_thrds = get_nr_threads(data->t);
+    if (!p->mm) {
+        seq_printf(s, "%8d %8d  0x%px 0x%px [%16s]\n", p->tgid, t->pid, t,
+                   t->stack, t->comm);
+    } else {
+        if ((p->tgid == t->pid) && (nr_thrds > 1)) {
+            seq_printf(s, "%8d %8d  0x%px 0x%px %16s  %3d\n", p->tgid, t->pid,
+                       t, t->stack, t->comm, nr_thrds);
+        } else {
+            seq_printf(s, "%8d %8d  0x%px 0x%px %16s\n", p->tgid, t->pid, t,
+                       t->stack, t->comm);
+        }
+    }
+    task_unlock(t);
+    put_task_struct(t);
+
     return 0;
 }
 
-static struct seq_operations tasks_seq_ops = {
-    .start = tasks_seq_start,
-    .next = tasks_seq_next,
-    .stop = tasks_seq_stop,
-    .show = tasks_seq_show,
+static struct seq_operations thrds_seq_ops = {
+    .start = thrds_seq_start,
+    .next = thrds_seq_next,
+    .stop = thrds_seq_stop,
+    .show = thrds_seq_show,
 };
 
-static int32_t tasks_seq_open(struct inode *inode, struct file *file)
+static int32_t thrds_seq_open(struct inode *inode, struct file *file)
 {
-    return seq_open(file, &tasks_seq_ops);
+    int32_t ret;
+    ret = seq_open(file, &thrds_seq_ops);
+    if (!ret) {
+        struct seq_file *seq = file->private_data;
+        seq->private = &reach_end;
+        rcu_read_lock();
+    }
+    pr_info(MODULE_TAG " open /proc/seq_thrds\n");
+    return ret;
 }
 
-static struct file_operations tasks_file_ops = {
+static int32_t thrds_seq_release(struct inode *inode, struct file *file)
+{
+    pr_info(MODULE_TAG " release /proc/seq_thrds\n");
+    reach_end = 0;
+    rcu_read_unlock();
+    return seq_release(inode, file);
+}
+
+static struct file_operations thrds_file_ops = {
     .owner = THIS_MODULE,
-    .open = tasks_seq_open,
+    .open = thrds_seq_open,
     .read = seq_read,
     .llseek = seq_lseek,
-    .release = seq_release,
+    .release = thrds_seq_release,
 };
 
 static int32_t __init cw_seqfile_test_init(void)
 {
     // /proc/seq_task
     if (!proc_create("seq_num", 0644, NULL, &num_file_ops)) {
-        pr_err(MODULE_TAG " create proc file failed!\n");
+        pr_err(MODULE_TAG " create /proc/seq_num failed!\n");
         return -ENOMEM;
     }
+
+    if (!proc_create("seq_thrds", 0644, NULL, &thrds_file_ops)) {
+        pr_err(MODULE_TAG "create /proc/seq_thrds failed!\n");
+        remove_proc_entry("seq_num", NULL);
+        return -ENOMEM;
+    }
+
     pr_info(MODULE_TAG " init successfully!\n");
     return 0;
 }
@@ -168,6 +310,7 @@ static int32_t __init cw_seqfile_test_init(void)
 static void __exit cw_seqfile_test_exit(void)
 {
     remove_proc_entry("seq_num", NULL);
+    remove_proc_entry("seq_thrds", NULL);
     pr_info(MODULE_TAG " bye!\n");
 }
 
