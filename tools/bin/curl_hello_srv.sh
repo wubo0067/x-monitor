13c2d85bfb0896fb18c779a91c0d4821d8d3df9e
calm.wu <wubo0067@hotmail.com>
2025-04-17 14:43:55 +0800

    feat: 新增 curl_hello_srv.sh 脚本以循环调用指定服务器，支持自定义循环次数 feat: 更新 hello_srv.py 脚本以返回随机数据，增强响应内容 feat: 更新 trace_qdisc.bt 脚本以增加 skb 入队时间跟踪和统计功能

---
+++ tools/bin/curl_hello_srv.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 设置默认的循环次数
+DEFAULT_LOOPS=5
+
+# 获取传入的循环次数，如果没有则使用默认值
+LOOPS=${1:-$DEFAULT_LOOPS}
+
+# 确保循环次数是正整数
+if ! [[ "$LOOPS" =~ ^[1-9][0-9]*$ ]]; then
+  echo "错误: 循环次数必须是正整数。"
+  echo "用法: $0 [循环次数]"
+  exit 1
+fi
+
+# 循环调用服务器
+echo "开始循环调用服务器 $LOOPS 次..."
+for ((i=1; i<=$LOOPS; i++)); do
+  echo "第 $i 次调用..."
+  curl -sS http://192.168.14.131:8000 > /dev/null
+  echo ""
+  #sleep 0.5 # 可选的延迟
+done
+
+echo "完成。"
--- tools/bin/hello_srv.py
+++ tools/bin/hello_srv.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3.9
+
 from time import sleep
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import random
+import string
+from http.server import ThreadingHTTPServer, BaseHTTPRequestHandler
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -8,10 +12,15 @@ class MyHandler(BaseHTTPRequestHandler):
             content_length = int(content_length)
             print(f"Request data length: {content_length}")
 
+        random_data = ''.join(random.choices(string.ascii_letters + string.digits, k=30720))
+        response_data = random_data.encode('utf-8')
+
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-Length', len(response_data))
         self.end_headers()
-        self.wfile.write(b'hello')
+        #sleep(6)
+        self.wfile.write(response_data)
 
-httpd = HTTPServer(('localhost', 8000), MyHandler)
+httpd = ThreadingHTTPServer(('0.0.0.0', 8000), MyHandler)
 httpd.serve_forever()
