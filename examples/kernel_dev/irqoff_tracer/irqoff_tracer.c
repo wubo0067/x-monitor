fa0ff7e8bf9a6e1f42faf60354a23de94fdfbd7c
calm.wu <wubo0067@hotmail.com>
2025-05-27 17:45:43 +0800

    feat: 添加高精度定时器支持，优化中断追踪功能

--- examples/kernel_dev/irqoff_tracer/irqoff_tracer.c
+++ examples/kernel_dev/irqoff_tracer/irqoff_tracer.c
@@ -26,6 +26,9 @@
 #include <linux/mm.h>
 #include <linux/percpu-refcount.h>
 #include <linux/delay.h>
+#include <linux/stacktrace.h>
+#include <linux/hrtimer.h>
+#include <linux/timer.h>
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0)
 #include <linux/sched.h>
@@ -81,11 +84,11 @@ struct per_cpu_stack_trace {
 */
 static bool __trace_enabled = false;
 /*
-* @brief 默认采样周期，10ms
+* @brief 默认采样周期，10ms，单位是纳秒
 */
 static uint64_t __sampling_period = 10 * 1000 * 1000UL;
 /*
-* @brief 默认追踪中断关闭的延迟，50ms
+* @brief 默认追踪中断关闭的延迟，50ms，单位是纳秒
 */
 static u64 __trace_irqoff_latency = 50 * 1000 * 1000UL;
 
@@ -93,21 +96,105 @@ static u64 __trace_irqoff_latency = 50 * 1000 * 1000UL;
 static struct per_cpu_stack_trace __percpu *__cpu_stack_trace;
 
 // ***************irqoff trace 开关********** */
-static int32_t __irqoff_trace_enabled_show(struct seq_file *seq, void *data)
+// 高精度定时器回调函数
+static enum hrtimer_restart
+        __irqoff_tracer_hrtimer_callback(struct hrtimer *hrtimer)
+{
+    // 重启定时器，间隔为采样周期
+    hrtimer_forward_now(hrtimer, ns_to_ktime(__sampling_period));
+    return HRTIMER_RESTART;
+}
+
+static void __irqoff_tracer_timer_callback(struct timer_list *timer)
+{
+    // 获取当前时间戳，纳秒
+    uint64_t now = local_clock();
+    uint64_t delta;
+
+    delta = now -
+            __this_cpu_read(__cpu_stack_trace->hardirq_trace.last_timestamp);
+    __this_cpu_write(__cpu_stack_trace->softirq_trace.last_timestamp, now);
+    __this_cpu_write(__cpu_stack_trace->softirq_delayed, false);
+}
+
+static void __irqoff_tracer_smp_start_timer(void *info)
+{
+    uint64_t now = local_clock();
+    struct per_cpu_stack_trace __percpu *trace =
+            (struct per_cpu_stack_trace *)info;
+
+    // 设置定时器启动时间
+    trace->hardirq_trace.last_timestamp = now;
+    trace->softirq_trace.last_timestamp = now;
+
+    // 允许你指定一个到期时间范围，而不是一个精确的到期时间点。这在某些情况下非常有用，
+    // 例如当对定时器的实际触发时间有一个可接受的微小波动范围时，可以允许内核在内部进行一些优化
+    hrtimer_start_range_ns(&(trace->hrtime), ns_to_ktime(__sampling_period), 0,
+                           HRTIMER_MODE_PINNED);
+    trace->timer.expires =
+            jiffies + msecs_to_jiffies(__sampling_period / 1000000UL);
+    add_timer_on(&trace->timer, smp_processor_id());
+}
+
+static void __irqoff_tracer_start_timers(void)
+{
+    int32_t cpu;
+
+    for_each_online_cpu (cpu) {
+        struct hrtimer *hrtimer;
+        struct timer_list *timer;
+        // 初始化高精度计时器
+        hrtimer = per_cpu_ptr(__cpu_stack_trace->hrtime, cpu);
+        // 定时器绑定到 cpu 上，回调函数只会在该 cpu 上执行，而不会被迁移
+        hrtimer_init(hrtimer, CLOCK_MONOTONIC, HRTIMER_MODE_PINNED);
+        hrtimer->function = __irqoff_tracer_hrtimer_callback;
+
+        // 初始化传统定时器
+        timer = per_cpu_ptr(__cpu_stack_trace->timer, cpu);
+        // TIMER_IRQSAFE 表示回调函数保证中断安全
+        timer_setup(timer, __irqoff_tracer_timer_callback,
+                    TIMER_PINNED | TIMER_IRQSAFE);
+
+        // 在指定 cpu 上启动定时器，等待函数执行完毕
+        smp_call_function_single(cpu, __irqoff_tracer_smp_start_timer,
+                                 per_cpu_ptr(__cpu_stack_trace, cpu), true);
+    }
+}
+
+static void __irqoff_tracer_stop_timers(void)
+{
+    int32_t cpu;
+
+    for_each_online_cpu (cpu) {
+        struct hrtimer *hrtimer;
+        struct timer_list *timer;
+
+        // 获取当前 CPU 的高精度计时器和传统定时器
+        hrtimer = per_cpu_ptr(__cpu_stack_trace->hrtime, cpu);
+        timer = per_cpu_ptr(__cpu_stack_trace->timer, cpu);
+
+        // 停止高精度计时器
+        hrtimer_cancel(hrtimer);
+        // 停止传统定时器
+        del_timer_sync(timer);
+    }
+}
+
+static int32_t __irqoff_tracer_enabled_show(struct seq_file *seq, void *data)
 {
     seq_printf(seq, "%s\n", __trace_enabled ? "enabled" : "disabled");
     return 0;
 }
 
-static int32_t __irqoff_trace_enabled_open(struct inode *inode,
-                                           struct file *file)
+static int32_t __irqoff_tracer_enabled_open(struct inode *inode,
+                                            struct file *file)
 {
-    return single_open(file, __irqoff_trace_enabled_show, inode->i_private);
+    return single_open(file, __irqoff_tracer_enabled_show, inode->i_private);
 }
 
-static int32_t __irqoff_trace_enabled_write(struct file *file,
-                                            const char __user *buf, size_t cnt,
-                                            loff_t *ppos)
+static int32_t __irqoff_tracer_enabled_write(struct file *file,
+                                             const char __user *buf, size_t cnt,
+                                             loff_t *ppos)
 {
     char kbuf[8]; // 足够存储 "true", "false", "0", "1", "on", "off", "y", "n" 等
     int ret;
@@ -138,35 +225,39 @@ static int32_t __irqoff_trace_enabled_write(struct file *file,
         return ret; // kstrtobool 返回负的 errno
     }
 
-    if (!!__trace_enabled != !!new_enabled) {
-        __trace_enabled = new_enabled;
-        pr_info(MODULE_TAG " tracing %s.\n",
-                (!!__trace_enabled) ? "enabled" : "disabled");
-        // TODO: 根据 __trace_enabled 的新值，启动或停止追踪逻辑
-        // 例如，如果 new_enabled 为 true，则启动定时器和追踪
-        // 如果 new_enabled 为 false，则停止定时器和追踪
-    } else {
+    // 如果设置相同，直接返回
+    if (!!__trace_enabled == !!new_enabled) {
         pr_info(MODULE_TAG " tracing already %s.\n",
                 (!!__trace_enabled) ? "enabled" : "disabled");
+        return cnt; // 没有变化，直接返回
     }
+
+    if (new_enabled) {
+        pr_info(MODULE_TAG " enabling tracing.\n");
+        __irqoff_tracer_start_timers();
+    } else {
+        pr_info(MODULE_TAG " disabling tracing.\n");
+        __irqoff_tracer_stop_timers();
+    }
+    __trace_enabled = new_enabled;
 
     *ppos += cnt; // 更新文件位置指针
     return cnt;   // 返回消耗的字节数
 }
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 6, 0)
-static const struct file_operations __irqoff_trace_enabled_fops = {
-    .open = __irqoff_trace_enabled_open,
+static const struct file_operations __irqoff_tracer_enabled_fops = {
+    .open = __irqoff_tracer_enabled_open,
     .read = seq_read,
-    .write = __irqoff_trace_enabled_write,
+    .write = __irqoff_tracer_enabled_write,
     .llseek = seq_lseek,
     .release = single_release,
 };
 #else
-static const struct proc_fops __irqoff_trace_enabled_fops = {
-    .proc_open = __irqoff_trace_enabled_open,
+static const struct proc_fops __irqoff_tracer_enabled_fops = {
+    .proc_open = __irqoff_tracer_enabled_open,
     .proc_read = seq_read,
-    .proc_write = __irqoff_trace_enabled_write,
+    .proc_write = __irqoff_tracer_enabled_write,
     .proc_lseek = seq_lseek,
     .proc_release = single_release,
 };
@@ -254,9 +345,21 @@ static int32_t __init __cw_irqoff_tracer_init(void)
 
     // TODO:trace_latency
 
-    // TODO:enable
+    if (!proc_create("enabled", 0644, parent_dir,
+                     &__irqoff_tracer_enabled_fops)) {
+        pr_err(MODULE_TAG " failed to create /proc/irqoff_tracer/enabled.\n");
+        goto err_remove_proc;
+    }
+    pr_info(MODULE_TAG " successfully created /proc/irqoff_tracer/enabled.\n");
 
-    // TODO:sampling_period
+    if (!proc_create("sampling_period", 0644, parent_dir,
+                     &__sampling_period_fops)) {
+        pr_err(MODULE_TAG
+               " failed to create /proc/irqoff_tracer/sampling_period.\n");
+        goto err_remove_proc;
+    }
+    pr_info(MODULE_TAG
+            " successfully created /proc/irqoff_tracer/sampling_period.\n");
 
     pr_info(MODULE_TAG " successfully created /proc/irqoff_tracer.\n");
     return 0;
@@ -271,6 +374,11 @@ err_free_percpu:
 
 static void __exit __cw_irqoff_tracer_exit(void)
 {
+    if (__trace_enabled) {
+        __irqoff_tracer_stop_timers();
+        __trace_enabled = false;
+    }
+    remove_proc_subtree("irqoff_tracer", NULL);
     free_percpu(__cpu_stack_trace);
     pr_info(MODULE_TAG " exited.\n");
 }
