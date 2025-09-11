2962442197f4249d7dd4a79d452876ef147f4344
calm.wu <wubo0067@hotmail.com>
2025-09-11 18:04:12 +0800

    feat: 添加 xm_sock_load 示例及其运行脚本，支持 BPF 程序的测试和验证

--- plugin_ebpf/bpf/xm_sock_flags.bpf.c
+++ plugin_ebpf/bpf/xm_sock_flags.bpf.c
@@ -12,6 +12,18 @@
 #define AF_INET6       10 /* IP version 6			*/
 #define IPPROTO_ICMPV6 58 /* ICMPv6			*/
 
+/*
+strace -e socket ping -c1 -w1 172.16.1.100
+socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP) = 3
+socket(AF_INET6, SOCK_DGRAM, IPPROTO_ICMPV6) = 4
+socket(AF_INET, SOCK_DGRAM, IPPROTO_IP) = 5
+
+
+strace -e socket ping6 -c1 -w1 2401:db00::1
+socket(AF_INET6, SOCK_DGRAM, IPPROTO_ICMPV6) = 3
+socket(AF_INET6, SOCK_DGRAM, IPPROTO_IP) = 4
+*/
+
 SEC("cgroup/sock1")
 int32_t xm_sock_prog1(struct bpf_sock *sk)
 {
@@ -19,17 +31,18 @@ int32_t xm_sock_prog1(struct bpf_sock *sk)
 	uint32_t uid = gid_uid & 0xFFFFFFFF;
 	uint32_t gid = gid_uid >> 32;
 
-	bpf_printk("socket: family:'%d', type:'%d', protocol:'%d'\n",
-		   sk->family, sk->type, sk->protocol);
-	bpf_printk("socket: uid:'%u', gid:'%u'\n", uid, gid);
+	bpf_printk("socket: family:'%d', type:'%d', protocol:'%d'", sk->family,
+		   sk->type, sk->protocol);
+	bpf_printk("socket: uid:'%u', gid:'%u'", uid, gid);
 
 	/*
 	block PF_INET6, SOCK_RAW, IPPROTO_ICMPV6 sockets
 	ie., make ping6 fail
 	*/
-	if (sk->family == AF_INET6 && sk->type == SOCK_RAW &&
+	if (sk->family == AF_INET6 &&
+	    (sk->type == SOCK_RAW || sk->type == SOCK_DGRAM) &&
 	    sk->protocol == IPPROTO_ICMPV6) {
-		bpf_printk("block PF_INET6, SOCK_RAW, IPPROTO_ICMPV6 socket\n");
+		bpf_printk("block PF_INET6, SOCK_RAW, IPPROTO_ICMPV6 socket");
 		// 返回 0 表示拒绝
 		return 0;
 	}
@@ -41,15 +54,16 @@ int32_t xm_sock_prog1(struct bpf_sock *sk)
 SEC("cgroup/sock2")
 int32_t xm_sock_prog2(struct bpf_sock *sk)
 {
-	bpf_printk("socket: family:'%d', type:'%d', protocol:'%d'\n",
-		   sk->family, sk->type, sk->protocol);
+	bpf_printk("socket: family:'%d', type:'%d', protocol:'%d'", sk->family,
+		   sk->type, sk->protocol);
 	/*
 	block PF_INET, SOCK_RAW, IPPROTO_ICMP sockets
 	ie. make ping fail
 	*/
-	if (sk->family == AF_INET && sk->type == SOCK_RAW &&
+	if (sk->family == AF_INET &&
+	    (sk->type == SOCK_RAW || sk->type == SOCK_DGRAM) &&
 	    sk->protocol == IPPROTO_ICMP) {
-		bpf_printk("block PF_INET, SOCK_RAW, IPPROTO_ICMP socket\n");
+		bpf_printk("block PF_INET, SOCK_RAW, IPPROTO_ICMP socket");
 		// 返回 0 表示拒绝
 		return 0;
 	}
--- plugin_ebpf/examples/xm_sock_load/main.go
+++ plugin_ebpf/examples/xm_sock_load/main.go
@@ -8,33 +8,94 @@
 package main
 
 import (
-	goflag "flag"
+	"flag"
+	"fmt"
 
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/link"
 	"github.com/cilium/ebpf/rlimit"
 	"github.com/golang/glog"
 )
 
 //go:generate env GOPACKAGE=main bpf2go -no-strip -cc clang XMCGSockProg ../../bpf/xm_sock_flags.bpf.c -- -I../../bpf/.output -I../../../extra/include/bpf -g -Wall -Werror -Wno-unused-function -D__TARGET_ARCH_x86
 
+const (
+	lnkCGPinPath = "/sys/fs/bpf/xm_sock_flags"
+)
+
+func selectProgram(objs *XMCGSockProgObjects, filterID int) (*ebpf.Program, error) {
+	switch filterID {
+	case 1:
+		return objs.XmSockProg1, nil
+	case 2:
+		return objs.XmSockProg2, nil
+	default:
+		return nil, fmt.Errorf("invalid progFilterID: %d", filterID)
+	}
+}
+
 var (
-	// cgroup path to attach
-	__cgrpPath string
-	// prog filter id
-	__progFilterID int = -1
+	// cgroupPath is the path to the cgroup to attach to
+	cgroupPath string
+	// progFilterID is the program filter ID
+	progFilterID int
 )
 
 func init() {
-	goflag.StringVar(&__cgrpPath, "cgrpPath", "/sys/fs/cgroup/unified/xm_sock_flags", "cgroup path to attach")
-	goflag.IntVar(&__progFilterID, "progFilterID", -1, "prog filter id")
+	flag.StringVar(&cgroupPath, "cgrpPath", "/tmp/cgroupv2/foo", "cgroup path to attach")
+	flag.IntVar(&progFilterID, "progFilterID", 1, "prog filter id")
 }
 
 func main() {
-	goflag.Parse()
+	var err error
+	var lnkCG link.Link
+
+	flag.Parse()
 	defer glog.Flush()
 
-	glog.Infoln("Starting xm_sock_flags program")
+	// 判断参数个数，如果参数数量不为 3，报错退出
+	if cgroupPath == "" || progFilterID <= 0 {
+		glog.Fatalf("Usage: xm_sock_flags -cgrpPath <cgroup path> -progFilterID <1/2>")
+	}
+
+	glog.Infof("Starting xm_sock_flags program, cgrpPath: %s, progFilterID: %d", cgroupPath, progFilterID)
 
 	if err := rlimit.RemoveMemlock(); err != nil {
 		glog.Fatal(err)
 	}
+
+	objs := XMCGSockProgObjects{}
+	if err = LoadXMCGSockProgObjects(&objs, nil); err != nil {
+		glog.Fatalf("loading objects: %v", err)
+	}
+	defer objs.Close()
+
+	// Select program based on filter ID
+	program, err := selectProgram(&objs, progFilterID)
+	if err != nil {
+		glog.Fatalf("Failed to select program: %v", err)
+	}
+
+	// Attach to cgroup
+	lnkCG, err = link.AttachCgroup(link.CgroupOptions{
+		Path:    cgroupPath,
+		Attach:  ebpf.AttachCGroupInetSockCreate,
+		Program: program,
+	})
+	if err != nil {
+		glog.Fatalf("Failed to attach cgroup: %v", err)
+	}
+	defer lnkCG.Close()
+
+	// Pin the link
+	if err = lnkCG.Pin(lnkCGPinPath); err != nil {
+		glog.Fatalf("pinning link: %v", err)
+	}
+
+	glog.Infof("Successfully attached program and pinned link to %s", lnkCGPinPath)
+
 }
+
+/*
+rm -rf /sys/fs/bpf/xm_sock_flags
+*/
